d6d2847cb3d09a63e10c2e2842c777c4e153d4fe
alisonhussey <alisonhussey@gmail.com>
2020-12-04 19:19:30 +0000

    Refactored code to make it Object Oriented

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
   title: title,
@@ -20,24 +13,16 @@ function newTask(title, description) {
   return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-
-
-// Print the state of a task to the console in a nice readable way
-
-
-// DRIVER CODE BELOW
-
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
-const task2 = newTask("Do Laundry", "😨"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2]
 
 
-task1.logState(); // Clean Cat Litter has not been completed
+task1.logState();
 task1.markCompleted();
-task1.logState(); // Clean Cat Litter has been completed
+task1.logState();
 
-task2.logState(); // Clean Cat Litter has not been completed
+task2.logState();
 task2.markCompleted();
 task2.logState();
 
